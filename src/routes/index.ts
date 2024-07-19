792c401102941ac183065202d2c41c354491e63f
黄铭锵 <345375378@qq.com>
2024-07-19 10:15:49 +0800

    增加flow模块

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
-import user from '../routes/api/user'
+import user from './api/user'
+import flow from './api/flow';
 
 
 const Api = function() {}
 
 Api.prototype.achieve = (router) => {
   router.use('/api/user', user);
+  router.use('/api/flow', flow);
 }
 
 export default Api;
